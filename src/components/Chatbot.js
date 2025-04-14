58c000ebe90ae44a2da3acb5a5e47df45c1e7ec6
saida stifi <saida27stifi@email.com>
2025-04-14 19:47:28 +0100

    first commit

--- src/components/Chatbot.js
+++ src/components/Chatbot.js
@@ -13,7 +13,9 @@ function Chatbot() {
   useEffect(() => {
     const fetchMessages = async () => {
       try {
-        const res = await axios.get('http://localhost:5000/api/messages');
+       // const res = await axios.get('http://localhost:5000/api/messages');
+        const res = await axios.get('http://backend-production-96af.up.railway.app/api/messages');
+
         setMessages(res.data);
       } catch (error) {
         console.error('Error loading messages:', error);
@@ -32,7 +34,7 @@ function Chatbot() {
     setLoading(true);
 
     try {
-      const res = await axios.post('http://localhost:5000/api/chat', {
+      const res = await axios.post('http://backend-production-96af.up.railway.app/api/chat', {
         message: input,
       });
       const botMessage = { role: 'bot', content: res.data.response };
