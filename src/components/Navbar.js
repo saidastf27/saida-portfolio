89cb7fc3e68b9d68ab3a6c5129c20fe8dac4990d
saida stifi <saida27stifi@email.com>
2025-04-28 01:16:44 +0100

    Remove git-filter-repo from git tracking

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { Link, useLocation } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+
 import "./Navbar.css";
 import { motion } from "framer-motion";
 
